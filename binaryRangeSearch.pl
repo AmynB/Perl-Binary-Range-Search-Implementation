24e1a575e4c37e5c98a5e5d9e688a3e9f84dc6e6
p-epsi <bains.amyn@gmail.com>
2020-08-08 16:15:28 -0400

    Update binaryRangeSearch.pl

--- binaryRangeSearch.pl
+++ binaryRangeSearch.pl
@@ -5,7 +5,7 @@ no warnings 'recursion';
 use Data::Dumper;
 
 #************************************************************************************************
-#Author: abains-src                                                                           *
+#Author: ab-src                                                                       *
 #Purpose: Range search algorithm exercise with references                                       *
 #************************************************************************************************
 
